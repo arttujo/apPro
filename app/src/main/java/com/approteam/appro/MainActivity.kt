ed28cba2dfe866790f5061257aa98eda0fa5f71e
magmake <markus.saronsalo@metropolia.fi>
2020-02-12 13:57:36 +0200

    mapActivity

--- app/src/main/java/com/approteam/appro/MainActivity.kt
+++ app/src/main/java/com/approteam/appro/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.approteam.appro
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    private val bundle = Bundle()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // request location permission
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.openMap)
+        btn.setOnClickListener {
+            Log.d("DBG", "Button pressed")
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
