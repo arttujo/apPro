770316330fd1ac6e3f8ea75216a21f07289b34a0
unknown <arttujo@gmail.com>
2020-02-12 16:56:00 +0200

    bottom nav tweaks, removed some files and added values-fi

--- app/src/main/java/com/approteam/appro/MainActivity.kt
+++ app/src/main/java/com/approteam/appro/MainActivity.kt
@@ -15,30 +15,30 @@ class MainActivity : AppCompatActivity() {
     private val bundle = Bundle()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        // request location permission
+
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
 
         bottom_navigation.setOnNavigationItemSelectedListener {menuitem ->
             when (menuitem.itemId){
                 R.id.navigation_home -> {
                     Log.d("DBG","Home clicked")
+
+                    return@setOnNavigationItemSelectedListener true
                 }
                 R.id.navigation_map -> {
                     Log.d("DBG", "Map clicked")
                     val intent = Intent(this, MapsActivity::class.java)
                     startActivity(intent)
+                    return@setOnNavigationItemSelectedListener true
                 }
                 R.id.navigation_scan -> {
                     Log.d("DBG", "Scan Clicked")
+                   return@setOnNavigationItemSelectedListener true
                 }
             }
          false
         }
     }
-
-
 }
