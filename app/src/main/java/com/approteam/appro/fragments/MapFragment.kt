74405d00ffda96765ae028b7548dbab3ea73587d
magmake <markus.saronsalo@metropolia.fi>
2020-02-13 16:33:28 +0200

    mapTweaks2

--- app/src/main/java/com/approteam/appro/fragments/MapFragment.kt
+++ app/src/main/java/com/approteam/appro/fragments/MapFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.approteam.appro.LocationListener
 import com.approteam.appro.R
@@ -38,8 +39,14 @@ class MapFragment(ctx: Context) : Fragment(), OnMapReadyCallback, LocationListen
         val view = inflater.inflate(R.layout.map_fragment, container, false)
         // Floating button for centering to user location
         val centerBtn = view.findViewById<FloatingActionButton>(R.id.btnCenter)
+        // Disable center button until first location is updated
+        centerBtn.isEnabled = false
         centerBtn.setOnClickListener {
+            // Center map to current location and zoom it
             mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(15.toFloat()))
+            Toast.makeText(this.context, "Centered to your location", Toast.LENGTH_SHORT)
+                .show()
             Log.d("DBG", "Map centered")
         }
 
@@ -57,17 +64,19 @@ class MapFragment(ctx: Context) : Fragment(), OnMapReadyCallback, LocationListen
         mMap = googleMap
         val zoomLevel: Float = 10.toFloat()
         // Zoom in map a bit
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(zoomLevel))
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(zoomLevel))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
         /*val helsinki = LatLng(60.19,24.94)
         mMap.addMarker(MarkerOptions().position(helsinki).title("Helsinki"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(helsinki))*/
-        btnCenter.isEnabled
         mapInitialized = true
     }
 
     override fun onLocationResults(lat: Double, lon: Double) {
+        // Wait for map to initialize before updating locations
         if (mapInitialized) {
             currentLocation = LatLng(lat, lon)
+            btnCenter.isEnabled = true
             mMap.addMarker(MarkerOptions().position(currentLocation).title("My Location"))
             Log.d("DBG", "MAP FRAGMENT RECEIVED LOCATION")
             Log.d("DBG", "$lat")
