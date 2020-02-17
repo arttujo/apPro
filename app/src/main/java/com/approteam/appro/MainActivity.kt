b30948d7f0655f71a479e48c82f162be11fef8a4
magmake <markus.saronsalo@metropolia.fi>
2020-02-17 14:17:42 +0200

    blinkingscantext

--- app/src/main/java/com/approteam/appro/MainActivity.kt
+++ app/src/main/java/com/approteam/appro/MainActivity.kt
@@ -49,6 +49,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
         permissionRequest()
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         supportFragmentManager.beginTransaction().add(R.id.container, homeFragment).commit()
--- app/src/main/java/com/approteam/appro/fragments/ScanFragment.kt
+++ app/src/main/java/com/approteam/appro/fragments/ScanFragment.kt
@@ -1,17 +1,25 @@
 package com.approteam.appro.fragments
 
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
 import android.os.Parcel
 import android.os.Parcelable
+import android.renderscript.Sampler
 import android.util.Log
 import android.util.SparseArray
 import android.view.*
+import android.view.animation.Animation
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.approteam.appro.MainActivity
@@ -21,14 +29,17 @@ import com.google.android.gms.vision.CameraSource
 import com.google.android.gms.vision.Detector
 import com.google.android.gms.vision.barcode.Barcode
 import com.google.android.gms.vision.barcode.BarcodeDetector
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
 class ScanFragment(ctx: Context) : Fragment() {
 
-    private lateinit var thisCode: Barcode
     private lateinit var detector: BarcodeDetector
     private lateinit var imageScanView: SurfaceView
     private lateinit var camera: CameraSource
     private lateinit var barCodes: SparseArray<Barcode>
     private lateinit var tV: TextView
+    private lateinit var blinkAnimation: ObjectAnimator
     var cameraEnabled = false
     private var c = ctx
     private val CAMERA_REQUEST_CODE = 200
@@ -40,7 +51,8 @@ class ScanFragment(ctx: Context) : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.scan_fragment, container, false)
-        tV = view.findViewById(R.id.barCodeInfo)
+        tV = view.findViewById(R.id.scanTV)
+        tV.text = getString(R.string.QR_search_text)
         imageScanView = view.findViewById(R.id.scanImage)
         requestCamera()
         detector = BarcodeDetector.Builder(c).setBarcodeFormats(Barcode.ALL_FORMATS).build()
@@ -70,7 +82,12 @@ class ScanFragment(ctx: Context) : Fragment() {
         imageScanView.holder.addCallback(object : SurfaceHolder.Callback2 {
             override fun surfaceRedrawNeeded(holder: SurfaceHolder?) {}
 
-            override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+            override fun surfaceChanged(
+                holder: SurfaceHolder?,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
             }
 
             override fun surfaceDestroyed(holder: SurfaceHolder?) {
@@ -79,6 +96,7 @@ class ScanFragment(ctx: Context) : Fragment() {
 
             override fun surfaceCreated(holder: SurfaceHolder?) {
                 camera.start(holder)
+                scanBlinkEffectEnable()
 
             }
         })
@@ -144,10 +162,28 @@ class ScanFragment(ctx: Context) : Fragment() {
             if (resultCode == 666) {
                 if (data != null) {
                     val barcode = data.getParcelableExtra<Barcode>("barcode")
-                    tV.text = barcode?.displayValue
+                    scanBlinkEffectDisable()
+                    tV.text = getString(R.string.QR_code_found)
                 } else {
                     tV.text = "No data"
                 }
+            }
+        }
+    }
+
+    private fun scanBlinkEffectEnable() {
+        blinkAnimation = ObjectAnimator.ofInt(tV, "textColor", Color.WHITE, Color.TRANSPARENT, Color.WHITE)
+        blinkAnimation.duration = 1000
+        blinkAnimation.repeatCount = ObjectAnimator.INFINITE
+        blinkAnimation.repeatMode = ObjectAnimator.REVERSE
+        blinkAnimation.start()
+    }
+    private fun scanBlinkEffectDisable() {
+        doAsync {
+            uiThread {
+                blinkAnimation.removeAllListeners()
+                blinkAnimation.end()
+                blinkAnimation.cancel()
             }
         }
     }
