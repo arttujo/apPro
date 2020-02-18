bf8e1022d3b0af29702431b604ec467ece319ae6
magmake <markus.saronsalo@metropolia.fi>
2020-02-18 16:34:40 +0200

    homelistviewOpensFragment

--- app/src/main/java/com/approteam/appro/fragments/ApproFragment.kt
+++ app/src/main/java/com/approteam/appro/fragments/ApproFragment.kt
@@ -1,27 +1,46 @@
 package com.approteam.appro.fragments
 
 import android.content.Context
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.approteam.appro.*
-import kotlinx.android.synthetic.main.home_fragment.*
+import com.google.android.gms.vision.barcode.Barcode
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.home_list_item.view.*
 
-class ApproFragment(ctx: Context): Fragment(){
+class ApproFragment(ctx: Context) : Fragment() {
 
+    private val c = ctx
+    private val picasso = Picasso.get()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ):
             View? {
-        val view = inflater.inflate(R.layout.appro_fragment,container,false)
-        return view
+        return inflater.inflate(R.layout.appro_fragment, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val aName = view.findViewById<TextView>(R.id.cardTitle)
+        val aImage = view.findViewById<ImageView>(R.id.cardImage)
+        val aDesc = view.findViewById<TextView>(R.id.cardDesc)
+        aName.text = arguments?.getString("approName")
+        val imageItem = arguments?.getString("approPic")
+        picasso.load(imageItem).into(aImage)
+        aDesc?.text = arguments?.getString("approDesc")
 
     }
 }
--- app/src/main/java/com/approteam/appro/fragments/HomeFragment.kt
+++ app/src/main/java/com/approteam/appro/fragments/HomeFragment.kt
@@ -1,40 +1,44 @@
 package com.approteam.appro.fragments
 
 import android.content.Context
-import android.net.Uri
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.ListAdapter
 import com.approteam.appro.*
-import kotlinx.android.synthetic.main.appro_fragment.*
+import com.google.android.gms.common.api.CommonStatusCodes
 import kotlinx.android.synthetic.main.home_fragment.*
 
-class HomeFragment(ctx: Context): Fragment(){
+class HomeFragment(ctx: Context) : Fragment() {
 
     private val c = ctx
 
     private val approFragment = ApproFragment(c)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.home_fragment,container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         approListRView.layoutManager = LinearLayoutManager(context)
         super.onViewCreated(view, savedInstanceState)
-        approListRView.adapter = HomeViewAdapter(Testdata.homeTestData,c){
-            approItemClicked(it)
+        approListRView.adapter = HomeViewAdapter(Testdata.homeTestData, c) {
+            val bundle = Bundle()
+            bundle.putString("approName", it.name)
+            bundle.putString("approDesc", it.desc)
+            bundle.putString("approPic", it.pic)
+            approFragment.arguments = bundle
+            activity?.supportFragmentManager?.beginTransaction()?.addToBackStack(null)
+                ?.replace(R.id.container, approFragment)?.commit()
         }
-
-    }
-    private fun approItemClicked(item: Sample) {
-        Log.d("DBG", "Clicked $item")
     }
 
 }
