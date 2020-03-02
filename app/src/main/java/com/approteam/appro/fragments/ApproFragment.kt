dc88ad861598c51af4b21db0caaf59ae9ffc456b
unknown <arttujo@gmail.com>
2020-03-02 11:45:04 +0200

    added lots of user experience improvements

--- app/src/main/java/com/approteam/appro/fragments/ApproFragment.kt
+++ app/src/main/java/com/approteam/appro/fragments/ApproFragment.kt
@@ -1,5 +1,6 @@
 package com.approteam.appro.fragments
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -18,8 +19,10 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.approteam.appro.*
+import com.approteam.appro.data_models.Appro
 import com.google.android.gms.vision.barcode.Barcode
 import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.appro_fragment.*
 
@@ -57,13 +60,60 @@ class ApproFragment(ctx: Context) : Fragment() {
         setupBundle()
         val json = arguments?.getString("approJson")
         Log.d("DBG","received: "+json)
+        buttonDisable(json!!)
         btnShowBars.setOnClickListener {
             activity?.supportFragmentManager?.beginTransaction()?.setCustomAnimations(android.R.anim.slide_in_left,android.R.anim.slide_out_right,android.R.anim.slide_in_left,android.R.anim.slide_out_right)?.addToBackStack(null)?.replace(R.id.container, barListFragment)?.commit()
         }
         btnJoinAppro.setOnClickListener {
-            approToSharedPrefs(arguments?.getString("approJson")!!)
+            if (!checkCurrentApproEquals(json)){ //If the appro isn't the same appro as in the preferences user will be alerted.
+                buildAlert(c,json)
+            } else {
+                approToSharedPrefs(arguments?.getString("approJson")!!)
+            }
         }
 
+    }
+    //Disables the join button if the user is already in the selected appro
+    private fun buttonDisable(approFromBundle: String){
+        val currApro= getCurrentApproData(c)
+        if (currApro != DEF_APPRO_VALUE) {
+            val currApproFromPrefs = Gson().fromJson(currApro, Appro::class.java)
+            val currApproFromBundle = Gson().fromJson(approFromBundle, Appro::class.java)
+
+            if (currApproFromBundle.name == currApproFromPrefs.name) {
+                btnJoinAppro.isEnabled = false
+                btnJoinAppro.isClickable = false
+                btnJoinAppro.text = getString(R.string.alreadyInAppro)
+            }
+        }
+    }
+    //Checks if the current appro being viewed matches the one in shared preferences
+    private fun checkCurrentApproEquals(approFromBundle: String):Boolean{
+        val currApro= getCurrentApproData(c)
+        var bool = true
+        if (currApro!= DEF_APPRO_VALUE) {
+            val currApproFromPrefs = Gson().fromJson(currApro, Appro::class.java)
+            val currApproFromBundle = Gson().fromJson(approFromBundle, Appro::class.java)
+            if (currApproFromBundle.name == currApproFromPrefs.name) {
+                Log.d("DBG", "EQUALS")
+                bool = true
+            } else if (currApproFromBundle.name != currApproFromPrefs.name) {
+                Log.d("DBG", "NOT EQUAL")
+                bool = false
+            }
+        }
+        return bool
+    }
+    //Alerts the user when they are about to join a new Appro when the old one is active
+    private fun buildAlert(ctx: Context,approString:String){
+        val builder = AlertDialog.Builder(ctx)
+        builder.setTitle(R.string.approReplacementWarn)
+        builder.setMessage(R.string.approReplacementMessage)
+        builder.setPositiveButton("Ok"){ dialog, which -> approToSharedPrefs(approString) }
+        builder.setNegativeButton(R.string.cancel){dialog, which -> Log.d("DBG","Cancelled replacement") }
+        val alert: AlertDialog = builder.create()
+        alert.setCancelable(true)
+        alert.show()
     }
 
     private fun setupBundle(){
@@ -95,7 +145,12 @@ class ApproFragment(ctx: Context) : Fragment() {
         toast.show()
     }
 
-
+    private fun getCurrentApproData(ctx: Context):String{
+        val mPrefs = ctx.getSharedPreferences(PREF_APPRO,Context.MODE_PRIVATE)
+        val approJsonString = mPrefs.getString(PREF_APPRO, DEF_APPRO_VALUE)
+        Log.d("DBG", "GOT APPRO: $approJsonString")
+        return approJsonString!!
+    }
 
 
 
